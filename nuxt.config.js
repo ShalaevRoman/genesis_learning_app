917508cf66a5171eb0d7703dde253fa0c6fa415f
ShalaevRoman <mc.shel24@gmail.com>
2023-03-19 00:28:00 +0200

    add gh-pages Dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 export default {
+  mode: 'static',
+  router: {
+    base: '/genesis_learning_app/'
+  },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
